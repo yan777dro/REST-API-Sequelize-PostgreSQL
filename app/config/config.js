f6f5212b38487ac3d832d067309e7def4ac3f96b
Yandro Portal <suarez.portal@gmail.com>
2021-05-31 11:10:30 -0500

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "Password123",
+    PASSWORD: "myPassword",
     DB: "employeesdb",
     dialect: "postgres",
     pool: {   
