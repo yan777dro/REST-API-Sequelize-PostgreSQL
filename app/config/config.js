e202f13739ad1cad405c51ec3c62c1899901d01c
Yandro <suarez.portal@gmail.com>
2022-09-15 20:16:07 -0500

    hello

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "myPassword",
+    PASSWORD: "Password123",
     DB: "employeesdb",
     dialect: "postgres",
     pool: {   
